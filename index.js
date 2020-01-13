3c115b2b8b7eb46fb9b25a9cf6b8adc8d5fdf9c4
Max <maxdeleers@gmail.com>
2020-01-13 14:04:31 +0100

    save final

---
+++ index.js
@@ -0,0 +1,73 @@
+// // 1. Projet
+
+// // 2. Le boolean
+// let maBoolean = true;
+
+// // 3. L'array
+// let monArray = "Bonjour";
+
+// // 4. L'objet
+// function monObjet(){
+
+// }
+
+// // 5. Le string
+// let monstring = "Hello the World"
+
+// // 6. 
+// var monInt = 32000
+
+// // 7. 
+// var Obje = new Object ()
+
+
+// // 8. La function
+// let double = n => n * 2;
+
+// // 9. La condition
+// if (monInt.lenght < 8) {
+//     console.log("C'est pas assez")
+// }
+// else{
+//     console.log("c'est trop!");
+// }
+
+// 10. La boucle
+let i = 1
+while (i < 0) {
+    y = i++
+}
+
+// 11. L'objet Date
+const date = new Date
+console.log(date);
+
+// 12. string to STRING
+// let nom = "Hello World";
+// console.log(nom.toUpperCase);
+
+// 13. string to StrinG
+let exo13 = "hello"
+
+
+// 14. La longueur
+
+// let exo14 = "Bonjour"
+// alert(exo14.length)
+
+// // 15. Le chiffre aléatoire p1
+// alert ( Math.round(Math.random()*101))
+
+// 16. Le chiffre aléatoire p2
+var saisie1;
+var saisie2;
+do{
+    saisie1 = prompt();
+    saisie2 = prompt()
+}while (alert(Math.round(Math.random(min*saisie1,max*saisie2)))) {
+}
+
+
+
+
+
